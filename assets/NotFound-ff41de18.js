2a30f9c6e08113ab24fc3bb0e3a1ba76a271af25
kirillsaint <kirillsaint@wtfuck.tk>
2023-10-06 16:57:46 +0000

    Deploying to gh-pages from @ Silent-Client/marketplace@f280ef46429bd459241b0c1fcbad9ad87a1c244d 🚀

--- assets/NotFound-ff41de18.js
+++
@@ -1,1 +0,0 @@
-import{j as n,C as t,S as e,H as o,L as i,q as a}from"./index-9ef963e5.js";function r(){return n.jsx(t,{p:5,children:n.jsxs(e,{textAlign:"center",direction:"column",spacing:2,children:[n.jsx(o,{children:"Page not found 😢"}),n.jsx(i,{opacity:"0.8",_hover:{textDecoration:"none",opacity:"1"},fontSize:"lg",as:a,to:"/",children:"Go to Home"})]})})}export{r as default};
